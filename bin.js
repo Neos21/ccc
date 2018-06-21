cd775242c099acc92c17d1d7c8361c6c2727c9ac
Neos21 <neos21@gmail.com>
2018-06-21 11:18:44 +0900

    [WIP]

--- bin.js
+++ bin.js
@@ -1,17 +1,5 @@
 #!/usr/bin/env node
 
-const lib = require('./lib.js');
+const ccc = require('./ccc.js');
 
-if(lib.checker.isColourCode(process.argv[2])) {
-  const parsed = lib.beautifier.beautifyColourCode(process.argv[2]);
-  const converted = lib.converter.colourCodeToRGBCode(parsed);
-  console.log(`ccc : [Colour Code] ${parsed} -> [RGB Code] ${converted}`);
-}
-else if(lib.checker.isRGBCode(process.argv[2], process.argv[3], process.argv[4])) {
-  const parsed = lib.beautifier.beautifyRGBCode(process.argv[2], process.argv[3], process.argv[4]);
-  const converted = lib.converter.rgbCodeToColourCode(parsed);
-  console.log(`ccc : [RGB Code] ${parsed} -> [Colour Code] ${converted}`);
-}
-else {
-  console.log('ccc : Invalid Arguments');
-}
+console.log(ccc(process.argv[2], process.argv[3], process.argv[4]) || 'Invalid Arguments');
